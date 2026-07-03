85cb49847bd59c030c2274c5591812579894f06a
Sakshyam Luitel <sakshyam73@gmail.com>
2026-07-03 14:59:47 +0545

    Initial Commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+function Page(){
+    return(
+        <>
+            <p>Invoices Page</p>
+        </>
+    )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function Page(){
+return(
+    <>
+        <p>Dashboard Page</p>
+    </>
+)   
+}
+
+export default Page
